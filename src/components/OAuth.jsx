d2ab47aa8d55e025974d0ffee2743be74f7db557
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-10-07 21:04:09 -0400

    add OAuth button

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
 
 export default function OAuth() {
   return (
     <>
-      <div>OAuth</div>
+      <button
+        className="flex items-center
+       justify-center w-full
+        bg-red-700 text-white
+         px-7 py-3 uppercase
+          text-sm font-md
+           hover:bg-red-800
+            active:red-900 rounded
+            shadow-md
+             hover:shadow-lg 
+             active:shadow-lg 
+             transition duration-150
+              ease-in-out"
+      >
+        <FcGoogle className="text-2xl bg-white rounded-full mr-8" />
+        Continue with Google
+      </button>
     </>
   );
 }
